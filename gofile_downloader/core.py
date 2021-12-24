02af8ff9bafd3fa6e9a8aeea68f646c865665e11
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2021-12-24 16:36:40 +0100

    Better request handling.

--- gofile_downloader/core.py
+++ gofile_downloader/core.py
@@ -1,27 +1,8 @@
 import os
-import ssl
 import json
 import hashlib
-import http.client as http_client
-import urllib.request as requests
+from .request import request, request_file
 import urllib.error as requests_error
-
-
-def http_get(url: str, headers: dict = { "User-Agent": "Mozilla/5.0" }) -> bytes:
-    http = requests.Request(url, headers=headers)
-    response = requests.urlopen(http)
-    chunk_limit = 100_000
-    data = response.read(chunk_limit)
-    while True:
-        try:
-            data += response.read(len(data)+chunk_limit)
-        except http_client.IncompleteRead as e:
-            data += e.partial
-            continue
-        else:
-            break
-    response.close()
-    return data
 
 
 def is_video(url: str) -> bool:
@@ -48,13 +29,14 @@ class GoFile:
         assert len(
             content_id) > 0, "An error occured while extracting the Content ID from '" + url + "'."
 
-        url = "https://api.gofile.io/getContent?contentId=" + content_id + "&token=" + self.api_key + "&websiteToken=websiteToken&cache=true"
+        url = "https://api.gofile.io/getContent?contentId=" + content_id + \
+            "&token=" + self.api_key + "&websiteToken=websiteToken&cache=true"
 
         if password is not None and password != '':
             password = hashlib.sha256(password.encode()).hexdigest()
             url += "&password=" + password
 
-        http = requests.Request(url, headers={
+        headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0",
             "Accept": "*/*",
             "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -67,13 +49,10 @@ class GoFile:
             "Sec-Fetch-Site": "same-site",
             "Pragma": "no-cache",
             "Cache-Control": "no-cache"
-        }, unverifiable=True)
+        }
+        data = request(url, headers=headers)
 
-        response = requests.urlopen(http, context=ssl._create_unverified_context())
-        data = response.read()
-        response.close()
-
-        resources =  json.loads(data.decode("utf-8"))
+        resources = json.loads(data.decode("utf-8"))
 
         links = []
         contents = resources["data"]["contents"]
@@ -88,35 +67,37 @@ class GoFile:
         if not isinstance(url, str) or len(url) < 1:
             raise ValueError("The URL must be a string.")
 
-        if skip_video and is_video(url): return
+        if skip_video and is_video(url):
+            return
 
         filename = url.split('/')[-1].split('?')[0]
         filename = filename.replace("%20", ' ')
-        if filename in os.listdir(output): return
+        if filename in os.listdir(output):
+            return
 
+        filename = os.path.join(output, filename)
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Cookie": "accountToken=" + self.api_key,
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "cross-site",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+        }
         try:
-            opener = requests.build_opener()
-            opener.addheaders = [
-                ("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"),
-                ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"),
-                ("Accept-Language", "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3"),
-                ("Accept-Encoding", "gzip, deflate, br"),
-                ("Connection", "keep-alive"),
-                ("Cookie", "accountToken=" + self.api_key),
-                ("Upgrade-Insecure-Requests", "1"),
-                ("Sec-Fetch-Dest", "document"),
-                ("Sec-Fetch-Mode", "navigate"),
-                ("Sec-Fetch-Site", "cross-site"),
-                ("Pragma", "no-cache"),
-                ("Cache-Control", "no-cache"),
-            ]
-            requests.install_opener(opener)
-            requests.urlretrieve(url, os.path.join(output, filename))
+            request_file(url, filename, headers=headers)
 
         except requests_error.ContentTooShortError:
-            data = http_get(url)
+            data = request(url)
 
-            with open(os.path.join(output, filename), "wb+") as s:
+            with open(filename, "wb+") as s:
                 s.write(data)
             s.close()
         except requests_error.HTTPError as e:
